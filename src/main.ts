62c6719b3b0838ee95679d06d829b4e8d9d1bbe0
Muin Momin <momin.muin@gmail.com>
2020-02-16 19:12:35 -0800

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import axios from 'axios'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const url: string = core.getInput('url');
+    core.info('Sending POST request...');
+    const data = JSON.parse(core.getInput('data'));
+    await axios.post(url, data);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
